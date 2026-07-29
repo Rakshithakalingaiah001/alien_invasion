0abd603836b6ad1e759515d57947abf86864f4c1
Rakshitha <rakshithakalingaiah@gmail.com>
2026-07-29 21:09:26 +0530

    Half process of the project alien_invasion

---
+++ settings.py
@@ -0,0 +1,12 @@
+class Settings:
+    """A class to store all settings for Alien Invasion."""
+
+    def __init__(self):
+        """Initialize the game's settings."""
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (255,255,255)  #sky blue color,(230,230,230) whitesh grey color,(255,255,255) pure white background.
+        self.ship_speed = 1.5  #ship settings.
+             
+        
