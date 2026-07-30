3742d2df84469c80eb4b68ea372bb84ad80edc67
Rakshitha <rakshithakalingaiah@gmail.com>
2026-07-30 22:58:22 +0530

    finished till ending the game

--- settings.py
+++ settings.py
@@ -7,6 +7,26 @@ class Settings:
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (255,255,255)  #sky blue color,(230,230,230) whitesh grey color,(255,255,255) pure white background.
-        self.ship_speed = 1.5  #ship settings.
-             
+
+        #Ship settings
+        self.ship_speed = 4  #ship settings.
+        self.ship_limit = 3
+
+        
+        #Bullet settings
+        self.bullet_speed = 3.0
+        self.bullet_width = 400 
+        self.bullet_height = 15
+        self.bullet_color = (60,60,60)
+        self.bullets_allowed = 3
+
+        #Alien settings
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        #fleet_direction of 1 represents right; -1 represents left.
+
+        self.fleet_direction = 1
+
+        
+
         
